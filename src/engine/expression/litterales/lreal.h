8474dc30a2155777b7ad4e2c40e1182425f6508b
Stephane_Branly <stephanebranly.pro@gmail.com>
2020-12-16 15:27:45 +0100

    rectif real

--- src/engine/expression/litterales/lreal.h
+++ src/engine/expression/litterales/lreal.h
@@ -7,13 +7,12 @@
 namespace Engine {
 class Lreal : public Lnumerical, public R1value
 {
-    float real;
+    double real;
 public:
     Lreal(const std::string s);
-    Lreal(float r): Lnumerical("Lreal"),real(r){};
-    Lreal* setReal(float r);
+    Lreal(double r): Lnumerical("Lreal"),real(r){};
+    Lreal* setReal(double r);
     double getValue() override {return real;}
-//    LReal* createCopy()const override{return new LReal(*this);}
     std::string const toString()const override{return std::to_string(real);}
     Expression* getCopy() const override { return new Lreal(real); }
     Expression* simplifyType() override;
