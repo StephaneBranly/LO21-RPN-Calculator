dabe4ca8cd4fda960403bbe5530989311ef01c18
Stephane_Branly <stephanebranly.pro@gmail.com>
2020-12-18 13:34:20 +0100

    fix heritage excepton

--- src/engine/exception/CompException.h
+++ src/engine/exception/CompException.h
@@ -1,19 +1,20 @@
 #ifndef COMPEXCEPTION_H
 #define COMPEXCEPTION_H
 #include <string>
-#include <exception>
+#include <stdexcept>
 
 namespace Engine {
 
 class ComputerException : public std::exception {
+protected:
     std::string info;
 public :
-    ComputerException(const std::string& str):info(str){}
+    ComputerException(const std::string& str="") noexcept:info(str){}
     const char* what() const noexcept {
            return info.c_str();
         }
+    ~ComputerException() noexcept{}
 };
-
 }
 
 #endif // COMPEXCEPTION_H
